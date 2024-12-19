4a328cdde1abd2455acc5ef854c6ed91703213fd
hilminugrah <hilminugrahs@gmail.com>
2024-12-19 10:45:47 +0700

    menambahkan gambar menu

--- salesApp/app.js
+++ salesApp/app.js
@@ -1,28 +1,35 @@
-const express = require("express")
+const express = require('express');
 const app = express();
 
-app.set("view engine", "ejs");
-app.set('views', "./views")
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.use(express.urlencoded({ extended: true }));
-app.use( express.static('public') )
+app.use(express.static('public'));
 
+app.get('/', function (req, res) {
+  res.render('home');
+});
 
+app.get('/grocery/home', function (req, res) {
+  res.render('v_grocery/grocery-home.ejs');
+});
 
-app.get('/' , function (req,res) {
-    res.render('home') 
-})
+app.get('/restaurant/home', function (req, res) {
+  res.render('v_restaurant/index.ejs');
+});
 
-app.get('/grocery/home', function (req,res) { 
-    res.render('v_grocery/grocery-home.ejs')
-} )
+app.get('/menu/home', function (req, res) {
+  res.render('v_restaurant/menu.ejs');
+});
 
-app.get('/restaurant/home', function (req,res) { 
-    res.render('v_restaurant/index.ejs')
-} )
+app.get('/login', function (req, res) {
+  res.render('v_authentication/login.ejs');
+});
 
-app.get('/menu/home', function (req,res) { 
-    res.render('v_restaurant/menu.ejs')
-} )
-app.listen(3000, ()=> {
-    console.log("server running on port 3000");
-})
+app.get('/register', function (req, res) {
+    res.render('v_authentication/register.ejs');
+  });
+
+app.listen(3000, () => {
+  console.log('server running on port 3000');
+});
