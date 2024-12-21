dbd3624fc2b48d23b6325c734a5c5974d60bf489
Dhiaulhaq <dhiaulhaq11@gmail.com>
2024-12-21 22:43:10 +0700

    applying mvc

--- salesApp/app.js
+++ salesApp/app.js
@@ -6,15 +6,13 @@ app.set('views', "./views")
 app.use(express.urlencoded({ extended: true }));
 app.use( express.static('public') )
 
-
-
 app.get('/' , function (req,res) {
     res.render('home') 
 })
 
 app.get('/grocery/home', function (req,res) { 
     res.render('v_grocery/grocery-home.ejs')
-} )
+})
 app.listen(3000, ()=> {
     console.log("server running on port 3000");
 })
